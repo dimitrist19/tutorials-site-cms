39d42ed4381b122b713523a0485c4bc61db2c9df
Dimitris T <56406468+dimitrist19@users.noreply.github.com>
2021-08-01 16:16:19 +0300

    Update tutorials.php

--- tutorials.php
+++ tutorials.php
@@ -1,9 +1,10 @@
 <?php
-include 'includes/check.php';
+require_once 'includes/check.php';
 require 'config.php';
-$result = mysqli_query($conn, "SELECT * FROM config WHERE id= 1");
-$row = mysqli_fetch_array($result);
-$tresult = mysqli_query($conn, "SELECT * FROM tutorials");
+$getSettings = mysqli_query($conn, "SELECT * FROM config WHERE id= 1");
+$getTutorials = mysqli_query($conn, "SELECT * FROM tutorials");
+
+$settings = mysqli_fetch_array($getSettings);
 ?>
 <html>
     <head>
@@ -13,10 +14,10 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="generator" content="MD">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-        <link rel="shortcut icon" href="<?= $row['faviconurl'] ?>" type="image/x-icon">
-        <meta name="description" content="<?= $row['hostname'] ?> | Web Hosting Video Tutorials">
+        <link rel="shortcut icon" href="<?= $settings['faviconurl'] ?>" type="image/x-icon">
+        <meta name="description" content="<?= $settings['hostname'] ?> | Web Hosting Video Tutorials">
 
-        <title><?= $row['hostname'] ?> - Video Tutorials</title>
+        <title><?= $settings['hostname'] ?> - Video Tutorials</title>
         <link rel="stylesheet" href="assets/css/mobirise-icons.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/socicon-styles.css">
@@ -39,7 +40,7 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
                 </button>
                 <div class="menu-logo">
                     <div class="navbar-brand">
-                        <span class="navbar-logo"><a href="index.php"><img src="<?= $row['logourl'] ?>" alt="Logo" style="height: 3.8rem;"></a></span>
+                        <span class="navbar-logo"><a href="index.php"><img src="<?= $settings['logourl'] ?>" alt="Logo" style="height: 3.8rem;"></a></span>
                         <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-1" href="index.php#header2-1"></a></span>
                     </div>
                 </div>
@@ -54,28 +55,28 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
         </section>
         <?php
         if (!isset($_GET['id'])) {
-            while ($row2 = mysqli_fetch_array($tresult)) {
+            while ($tutorial = mysqli_fetch_array($getTutorials)) {
                 echo '<section class="header7 cid-rEMpe1dCB9">
     <div class="container">
         <div class="media-container-row">
 
             <div class="media-content align-right">
-                <h1 class="mbr-section-title mbr-white pb-3 mbr-fonts-style display-1">' . $row2['title'] . '</h1>
+                <h1 class="mbr-section-title mbr-white pb-3 mbr-fonts-style display-1">' . $tutorial['title'] . '</h1>
                 <div class="mbr-section-text mbr-white pb-3">
-                    <p class="mbr-text mbr-fonts-style display-5">' . $row2['description'] . '</p>
+                    <p class="mbr-text mbr-fonts-style display-5">' . $tutorial['description'] . '</p>
                 </div>
-                <div class="mbr-section-btn"><a class="btn btn-md btn-primary display-5" href="tutorials.php?id=' . $row2['id'] . '">Read More</a></div>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-primary display-5" href="tutorials.php?id=' . $tutorial['id'] . '">Read More</a></div>
             </div>
-            <div class="mbr-figure" style="width: 100%;">' . $row2['videourl'] . '</div>
+            <div class="mbr-figure" style="width: 100%;">' . $tutorial['videourl'] . '</div>
             </div>
         </div>
     </div>
 </section>';
             }
         } else if (isset($_GET['id'])) {
-            $iresult = mysqli_query($conn, "SELECT * FROM tutorials WHERE id = {$_GET['id']}");
-            $row3 = mysqli_fetch_array($iresult);
-            if (is_null($row3['id'])) {
+            $getTutorialById = mysqli_query($conn, "SELECT * FROM tutorials WHERE id = {$_GET['id']}");
+            $singleTutorial = mysqli_fetch_array($getTutorialById);
+            if (is_null($singleTutorial['id'])) {
                 echo '<section class="header7 cid-rEMpe1dCB9">
     <div class="container">
         <div class="media-container-row">
@@ -98,26 +99,26 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
         <div class="media-container-row">
             <div class="media-content align-center">
                 <a href="tutorials.php"><font color="white"><i class="fa fa-arrow-left"></i> Back to all tutorials</font></a>
-                <h1 class="mbr-section-title mbr-white pb-3 mbr-fonts-style display-1">' . $row3['title'] . '</h1>
+                <h1 class="mbr-section-title mbr-white pb-3 mbr-fonts-style display-1">' . $singleTutorial['title'] . '</h1>
                 <div class="mbr-section-text mbr-white pb-3">
-                    <p class="mbr-text mbr-fonts-style display-5">' . $row3['description'] . '</p>
+                    <p class="mbr-text mbr-fonts-style display-5">' . $singleTutorial['description'] . '</p>
                 </div>
             </div>
             </div>
-            <div class="mbr-figure" style="width: 100%;">' . $row3['videourl'] . '</div>
+            <div class="mbr-figure" style="width: 100%;">' . $singleTutorial['videourl'] . '</div>
         </div>
     </div>
 </section>';
                 ?>
                 <?php
-                if (!isset($row3['body'])) {
+                if (!isset($singleTutorial['body'])) {
                     
-                } else if (is_null($row3['body'])) {
+                } else if (is_null($singleTutorial['body'])) {
                     
-                } else if (empty($row3['body'])) {
+                } else if (empty($singleTutorial['body'])) {
                     
                 } else {
-                    echo '<div style="padding: 75px;">' . $row3['body'] . '</div>';
+                    echo '<div style="padding: 75px;">' . $singleTutorial['body'] . '</div>';
                 }
             }
         }
@@ -129,7 +130,7 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
                     <div class="col-12 col-md-3">
                         <div class="media-wrap">
                             <a href="index.php">
-                                <img src="<?= $row['logourl'] ?>" alt="Logo">
+                                <img src="<?= $settings['logourl'] ?>" alt="Logo">
                             </a>
                         </div>
                     </div>
@@ -137,7 +138,7 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
                     </div>
                     <div class="col-12 col-md-3 mbr-fonts-style display-7">
                         <h5 class="pb-3">Links</h5>
-                        <p class="mbr-text"><a href="<?= $row['homeurl'] ?>">Homepage</a><br><a href="<?= $row['supporturl'] ?>" target="_blank">Support</a><br><a href="<?= $row['knowledgebaseurl'] ?>" target="_blank">Knowledgebase</a><br></p>
+                        <p class="mbr-text"><a href="<?= $settings['homeurl'] ?>">Homepage</a><br><a href="<?= $settings['supporturl'] ?>" target="_blank">Support</a><br><a href="<?= $settings['knowledgebaseurl'] ?>" target="_blank">Knowledgebase</a><br></p>
                     </div>
                     <div class="col-12 col-md-3 mbr-fonts-style display-7">
                         <h5 class="pb-3"></h5>
@@ -153,7 +154,7 @@ $tresult = mysqli_query($conn, "SELECT * FROM tutorials");
                     <div class="media-container-row mbr-white">
                         <div class="col-sm-6 copyright">
                             <p class="mbr-text mbr-fonts-style display-7">
-                                © Copyright 2021 <b><a href="<?= $row['homeurl'] ?>"><?= $row['hostname'] ?></a></b> - Tutorials Site CMS by <b><a href="https://github.com/dimitrist19">Dimitris T.</a></b>
+                                © Copyright 2021 <b><a href="<?= $settings['homeurl'] ?>"><?= $settings['hostname'] ?></a></b> - Tutorials Site CMS by <b><a href="https://github.com/dimitrist19">Dimitris T.</a></b>
                             </p>
                         </div>
                         <div class="col-md-6">
