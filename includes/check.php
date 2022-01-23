f3349f8434f683855a6c1490cf3b88bcd03b611d
Dimitris T <56406468+dimitrist19@users.noreply.github.com>
2022-01-23 10:01:23 +0200

    Update check.php
    Fix to redirect based on directory
    
    Before from '/admin' would redirect to '/admin/install.php'
    
    Now it should redirect from '/admin' to '/install.php'

--- includes/check.php
+++ includes/check.php
@@ -1,8 +1,14 @@
 <?php
 
+function redirect_by_path($path)
+{
+    $redirect = substr(strtr(realpath($path), '\\', '/'), strlen($_SERVER['DOCUMENT_ROOT']));
+    header("location: $redirect");
+    exit;
+}
+
 if (file_exists(__DIR__ . '/../install.php')) {
-    header('Location: install.php');
-    die;
+    redirect_by_path(__DIR__ . '/../install.php');
 }
 
 if (!file_exists('config.php') && !file_exists(__DIR__ . '/../config.php')) {
